e64fa6bfe8d70f53666a69cd4189a554367183ca
King-David <ajanakingdavid@outlook.com>
2025-04-30 15:07:12 +0100

    'packages'

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+ignore = '-e .'
+def get_requirements(file):
+    try:
+        with open(file) as file:
+            requirements = [req.strip() for req in file.readlines()]
+            if ignore in requirements:
+                requirements.remove(ignore)
+        return requirements
+    
+    except FileNotFoundError:
+        print("Requirements.txt not found")
+        return []
+    
+
+setup(
+    name='Ajana King-David',
+    version='0.01',
+    description='Customer Segmentation Project',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
